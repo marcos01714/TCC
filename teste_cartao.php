a9a8193681df5aa0963601efcca8abb42e74dd80
Marcos Vinicius <mv.sanchescardoso@gmail.com>
2025-06-24 22:01:09 -0300

    Add files via upload

---
+++ teste_cartao.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Teste de Envio de UID</title>
+</head>
+<body>
+  <h2>Simular envio de UID para cartao_leitura.php</h2>
+  <form action="cartao_leitura.php" method="post">
+    <label for="uid">UID do cartão:</label>
+    <input type="text" name="uid" value="ABC123">
+    <button type="submit">Enviar</button>
+  </form>
+</body>
+</html>
